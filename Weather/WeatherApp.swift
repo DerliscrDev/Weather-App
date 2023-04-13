a3288d7221ae126dc761227bc5dd061877110a82
Derlis Cañete <derliscrdev@gmail.com>
2023-04-13 19:02:20 -0400

    first commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Derlis Ramón Cañete Ríos on 2023-04-13.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
