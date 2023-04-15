62e6f389d4bb9e64f3e648eff0a1040803551743
Derlis Cañete <derliscrdev@gmail.com>
2023-04-14 21:41:58 -0400

    Add weather view model

---
+++ Weather/ViewModel/WeatherViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherViewModel.swift
+//  Weather
+//
+//  Created by Derlis Ramón Cañete Ríos on 2023-04-13.
+//
+
+import Foundation
+
+final class WeatherViewModel {
+    
+    func getWeather(city: String) async {
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=135935d4fd0889e153b14b90b2e8e0b3&units=metric&lang=es)")!
+        
+        do {
+            async let (data, _) = try await URLSession.shared.data(from: url)
+            let dataModel = try await JSONDecoder().decode(ResponseDataModel.self, from: data)
+            print(dataModel)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
